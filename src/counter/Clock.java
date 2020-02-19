e7e7c13b13eb5b4ece3ea8cc261c4ee627d43b96
Jacques Scheire <jscheire08@gmail.com>
2020-02-19 14:23:17 -0500

    finishing up time incrementer

--- src/counter/Clock.java
+++ src/counter/Clock.java
@@ -1,5 +1,24 @@
 package counter;
 
 public class Clock {
+	
+	public int hours;
+	public int minutes;
+	public int seconds;
+
+	public Clock(int hours, int minutes, int seconds) {
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+	}
+	
+	public String timeFormat() {
+		String hourString = String.format("%02d", hours);
+		String minuteString = String.format("%02d", minutes);
+		String secondString = String.format("%02d,", seconds);
+		return (hourString + ":" + minuteString + ":" + secondString);
+	}
+
+		
 
 }
--- src/counter/TimeIncrementer.java
+++ src/counter/TimeIncrementer.java
@@ -1,5 +1,50 @@
 package counter;
 
 public class TimeIncrementer {
-
+	
+	public Clock setTime(int hour, int minute, int second) {
+		return new Clock(hour, minute, second);
+	}
+	
+	public void displayTime(Clock currentTime) {
+		System.out.println(currentTime.timeFormat());
+	}
+	
+	public Clock incrementHour(Clock currentTime) {
+		Clock newTime = new Clock(currentTime.hours+1, currentTime.minutes, currentTime.seconds);
+		return newTime;
+		if(newTime.isMilitary() && newTime.hours == 24) {
+			return new Clock(0,0,0);
+		}
+	}
+	
+	public Clock incrementMinutes(Clock currentTime) {
+		Clock newTime = new Clock(currentTime.hours, currentTime.minutes+1, currentTime.seconds);
+		if(newTime.minutes == 60) {
+			newTime.hours++;
+			newTime.minutes = 0;
+			if(newTime.isMilitary() && newTime.hours == 24) {
+					return new Clock(0,0,0);
+				}
+			}
+		return newTime;
+	}
+	
+	public Clock incrementSeconds(Clock currentTime) {
+		Clock newTime = new Clock(currentTime.hours, currentTime.minutes, currentTime.seconds+1);
+		if(newTime.seconds == 60) {
+			newTime.minutes++;
+			newTime.seconds = 0;
+			if(newTime.minutes == 60) {
+				newTime.hours++;
+				newTime.minutes = 0;
+				if(newTime.isMilitary() && newTime.hours == 24) {
+						return new Clock(0,0,0);
+					
+				}
+			}
+		}
+		return newTime;
+	}
+	
 }
